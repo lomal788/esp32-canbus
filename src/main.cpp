cb92d2d04f4d954c23f4312edf7c9c7abf5e42a9
lomal788 <lomal788@gmail.com>
2023-08-18 22:25:56 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,36 +4,72 @@
 #include "freertos/task.h"
 #include "freertos/semphr.h"
 #include "esp_err.h"
+#include "esp_timer.h"
+#include <string.h>
+
+uint8_t msg_counter = 0;
+
+inline void to_bytes(uint64_t src, uint8_t* dst) {
+    for(uint8_t i = 0; i < 8; i++) {
+        dst[7-i] = src & 0xFF;
+        src >>= 8;
+    }
+}
 
 /* --------------------------- Tasks and Functions -------------------------- */
 static void rx_task_loop(void *arg){
+  twai_message_t message;
+  twai_message_t tx_can;
+  twai_status_info_t can_status;
+  uint64_t tmp;
+  
+
   while (true) {
-    twai_message_t message;
-    twai_status_info_t can_status;
     // esp_err_t aa = 
+    uint64_t now = esp_timer_get_time() / 1000;
     twai_get_status_info(&can_status);
     uint8_t f_count  = can_status.msgs_to_rx;
-    // printf("%d \n",aa);
 
     if (f_count == 0) {
-        vTaskDelay(4 / portTICK_PERIOD_MS); // Wait for buffer to have at least 1 frame
-      }else{
+      if( now % 2000 == 0){
+        printf("\n");
+        printf("TWAI Status: %d \n", can_status.state);
+        printf("TWAI Messages to Receive: %lu \n", can_status.msgs_to_rx);
+        printf("TWAI Messages to Send: %lu \n", can_status.msgs_to_tx);
+        printf("TWAI Messages Receive Errors: %lu \n", can_status.rx_error_counter);
+        printf("TWAI Messages Receive Missed: %lu \n", can_status.rx_missed_count);
+        printf("TWAI Messages Bus errors: %lu \n", can_status.bus_error_count);
+        printf("TWAI Messages ARB Lost: %lu \n", can_status.arb_lost_count);
+
+        printf("TWAI statuss: %d \n", twai_get_status_info(&can_status));
+        printf("TWAI rx error: %d \n", twai_receive(&message, pdMS_TO_TICKS(0)));
+
+        // memset(&tx_can, 0x00, sizeof(twai_message_t));
+
+        // tx_can.data_length_code = 8;
+        // tx_can.identifier = 0x07E9;
+        // tx_can.extd = 0;
+        // tx_can.rtr = 0;
+        // tx_can.ss = 1; // Always single shot
+        // tx_can.self = 0;
+        // tx_can.dlc_non_comp = 0;
+
+        // twai_transmit(&tx_can, 5);
+      }
+
+      vTaskDelay(4 / portTICK_PERIOD_MS); // Wait for buffer to have at least 1 frame
+    }else{
         printf("%d \n",f_count);
         if (twai_receive(&message, pdMS_TO_TICKS(0)) == ESP_OK) {
-          //printf("Message received\n");
-          // Process received message
-        /* if (message.extd){
-            printf("Message is in Extended Format\n");
-          }else{
-            printf("Message is in Standard Format\n");
-          }*/
-          // printf("0x%03x :", message.identifier);
-          if (!(message.rtr)){
-            for (int i = 0; i < message.data_length_code; i++){
-              printf("0x%02x ", message.data[i]);
-            }
-            printf("\n");
+          printf("Message received\n");
+
+          tmp = 0;
+          for (int i = 0; i < message.data_length_code; i++){
+            tmp |= (uint64_t)message.data[i] << (8*(7-i));
           }
+
+          printf("%lu %d %llu %llu ", message.identifier, message.data_length_code, tmp, now);
+          printf("\n");
         }
         vTaskDelay(2 / portTICK_PERIOD_MS); // Reset watchdog here
     }
