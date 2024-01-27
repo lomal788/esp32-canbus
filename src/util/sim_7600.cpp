50409a2325c5d59ade86239dc3041797b0145285
lomal788 <lomal788@gmail.com>
2024-01-27 22:36:27 +0900

    Update sim_7600.cpp

--- src/util/sim_7600.cpp
+++ src/util/sim_7600.cpp
@@ -5,6 +5,7 @@
 #include "my_config.h"
 #include "driver/uart.h"
 #include <driver/gpio.h>
+#include "sim_7600.h"
 
 #include <esp_system.h>
 #include <esp_event.h>
@@ -20,8 +21,7 @@ static const int RX_BUF_SIZE = 1024;
 #define TXD_PIN (GPIO_NUM_14)
 #define RXD_PIN (GPIO_NUM_27)
 
-#define UART UART_NUM_2
-
+// #define UART UART_NUM_2
 
 char *replaceAll(char *s, const char *olds, const char *news) {
   char *result, *sr;
@@ -86,20 +86,123 @@ void init_sim(){
   // uart_enable_rx_intr(UART);
 }
 
-static void tx_task(void *arg){
+void http_req(){
+  sendSimATCmd("AT+HTTPINIT");
+  sendSimATCmd("AT+HTTPPARA=\"URL\", \"http://webhook.site/991d65bd-ce0d-4d4d-8cdc-16ec65795d73\"");
+  sendSimATCmd("AT+HTTPACTION=0");
+  sendSimATCmd("AT+HTTPREAD?");
+  sendSimATCmd("AT+HTTPREAD=0,140");
+}
+
+void init_mqtt(){
+  sendSimATCmd("AT+CGREG?");
+  vTaskDelay(500 / portTICK_PERIOD_MS);
+  sendSimATCmd("AT+CIPMODE=1");
+  vTaskDelay(500 / portTICK_PERIOD_MS);
+  sendSimATCmd("AT+CSOCKSETPN=1");
+  vTaskDelay(500 / portTICK_PERIOD_MS);
+  sendSimATCmd("AT+CIPMODE=0");
+  vTaskDelay(500 / portTICK_PERIOD_MS);
+  sendSimATCmd("AT+NETOPEN");
+  vTaskDelay(500 / portTICK_PERIOD_MS);
+  sendSimATCmd("AT+CIPMODE=0");
+  vTaskDelay(500 / portTICK_PERIOD_MS);
+  sendSimATCmd("AT+CMQTTCONNECT?");
+  vTaskDelay(500 / portTICK_PERIOD_MS);
+  sendSimATCmd("AT+CMQTTSTART");
+  vTaskDelay(500 / portTICK_PERIOD_MS);
+  sendSimATCmd("AT+CMQTTACCQ=0,\"SIM7600_client\"");
+  vTaskDelay(500 / portTICK_PERIOD_MS);
+}
+
+void disconnect_mqtt(){
+//   AT+CMQTTDISC=0,120 
+// // release the client
+// AT+CMQTTREL=0
+// //stop MQTT Service
+// AT+CMQTTSTOP
+// AT+CMQTTDISC
+}
+
+void connect_mqtt_server(){
+  sendSimATCmd("AT+CMQTTCONNECT=0,\"tcp://broker.mqtt.cool:1883\",60,1");
+  vTaskDelay(500 / portTICK_PERIOD_MS);
+  sendSimATCmd("AT+CMQTTSUB=0,9,1,1");
+  vTaskDelay(500 / portTICK_PERIOD_MS);
+  sendSimATCmd("test/1234");
+}
+
+void sendSimATMsg(const char* cmd){
   char* Txdata = (char*) malloc(100);
 
+  sprintf(Txdata, "%s",cmd);
+  const int lengthaa = uart_write_bytes(UART, Txdata, strlen(Txdata));
+  if(lengthaa > 0) ESP_LOGI(TAG, "sent %d\n", lengthaa);
+  free(Txdata);
+}
+
+
+void sendSimATCmd(const char* cmd){
+  char* Txdata = (char*) malloc(100);
+
+  sprintf(Txdata, "%s\r\n",cmd);
+  const int lengthaa = uart_write_bytes(UART, Txdata, strlen(Txdata));
+  // if(lengthaa > 0) ESP_LOGI(TAG, "sent %d\n", lengthaa);
+  free(Txdata);
+}
+
+void subscribe_mqtt(const char* nm){
+  const char* sub_cmd = (char*) malloc(100);
+
+  sprintf(sub_cmd, "AT+CMQTTSUB=0,%d,1,1", strlen(nm));
+  sendSimATCmd(sub_cmd);
+  vTaskDelay(50 / portTICK_PERIOD_MS);
+  sendSimATMsg(nm);
+}
+
+char* getMqttResponse(){
+  uint8_t* rx_buffer = (uint8_t*) malloc(RX_BUF_SIZE+1);
+  const int rxBytes = uart_read_bytes(UART, rx_buffer, RX_BUF_SIZE, 500 / portTICK_PERIOD_MS);
+  
+  if (rxBytes > 0) {
+    rx_buffer[rxBytes] = 0;
+    // ESP_LOGI(RX_TASK_TAG, "Read %d bytes: '%s'\n", rxBytes, rx_buffer);
+  }
+  return (char*)rx_buffer;
+}
+
+void send_topic_mqtt(const char* nm, const char* msg){
+
+  char* topic_cmd = (char*) malloc(100);
+  sprintf(topic_cmd, "AT+CMQTTTOPIC=0,%d", strlen(nm));
+  sendSimATCmd((const char*)topic_cmd);
+  vTaskDelay(50 / portTICK_PERIOD_MS);
+  sendSimATMsg(nm);
+  vTaskDelay(50 / portTICK_PERIOD_MS);
+
+  char* payload_cmd = (char*) malloc(100);
+  sprintf(payload_cmd, "AT+CMQTTPAYLOAD=0,%d", strlen(msg));
+
+  sendSimATCmd((const char*)payload_cmd);
+  vTaskDelay(50 / portTICK_PERIOD_MS);
+  sendSimATMsg(msg);
+  vTaskDelay(50 / portTICK_PERIOD_MS);
+  sendSimATCmd("AT+CMQTTPUB=0,1,60");
+  // char* res = getMqttResponse();
+  // // +CMQTTPUB: 0,0
+  // ESP_LOGI(TAG, "Read : '%s' \n", res);
+}
+
+void tx_task(void *arg){
+
   while(1){
-    sprintf(Txdata, "AT+GMR\r\n");
-    const int lengthaa = uart_write_bytes(UART, Txdata, strlen(Txdata));
-    if(lengthaa > 0) ESP_LOGI(TAG, "sent %d\n", lengthaa);
+    sendSimATCmd("AT+GMR");
     vTaskDelay(2000 / portTICK_PERIOD_MS);
   }
-  free(Txdata);
   vTaskDelete(NULL);
 }
 
-static void rx_task(void *arg){
+void rx_task(void *arg){
   static const char *RX_TASK_TAG = "RX_TASK";
   esp_log_level_set(RX_TASK_TAG, ESP_LOG_INFO);
   uint8_t* rx_buffer = (uint8_t*) malloc(RX_BUF_SIZE+1);
