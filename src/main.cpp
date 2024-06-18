024f142de09eb4bad6d9f04a887e59b6b1a0d0e1
lomal788 <lomal788@gmail.com>
2024-06-18 23:11:22 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,9 @@
 #include <freertos/task.h>
 #include <freertos/semphr.h>
 
+#include "driver/uart.h"
 #include <driver/gpio.h>
 #include <driver/adc.h>
-#include "driver/uart.h"
 
 #include <esp_system.h>
 #include <esp_event.h>
@@ -23,7 +23,28 @@
 
 extern "C" void app_main(void) {
 
-  init_twai();
+  // init_twai();
 
-  xTaskCreate(&twai_rx_task, "can_rx_task", 8192, NULL, 5, NULL);
+  // xTaskCreate(&twai_rx_task, "can_rx_task", 8192, NULL, 5, NULL);
+
+
+  // wifi_connection();
+  // mqtt_start();
+
+  // int data_read = esp_http_client_read(client, ota_write_data, BUFFSIZE);
+
+  // if (data_read > 0) {
+  // }
+
+  // init_sim();
+  // init_mqtt();
+  // connect_mqtt_server();
+  // subscribe_mqtt("test/1234");
+
+  // xTaskCreate(&rx_task, "can_rx_task", 1024 * 8, NULL, 6, NULL);
+  // xTaskCreate(&call_sim_spam_task, "call_spam_task", 1024*4, NULL, 5, NULL);
+
+  // init_twai();
+  // xTaskCreate(&twai_rx_task, "can_rx_task", 8192, NULL, 6, NULL);
+
 }
