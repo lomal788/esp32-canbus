982750b5d530662eb6b900f3a83ddb776ff8d4e7
lomal788 <lomal788@gmail.com>
2024-06-18 23:10:23 +0900

    Update sim_7600.h

--- src/util/sim_7600.h
+++ src/util/sim_7600.h
@@ -7,12 +7,15 @@
 
 
 void init_sim();
+void init_mqtt();
+void connect_mqtt_server();
 void sendSimATMsg(const char* cmd);
 void sendSimATCmd(const char* cmd);
 
 void send_topic_mqtt(const char* nm, const char* msg);
-// void subscribe_mqtt(const char* nm);
+void subscribe_mqtt(const char* nm);
 
+void call_sim_spam_task(void *arg);
 void rx_task(void *arg);
 void tx_task(void *arg);
 
