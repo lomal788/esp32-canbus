047d81ea805ae299ac59d1fdd9b45108d4a97c89
lomal788 <lomal788@gmail.com>
2024-07-20 15:06:49 +0900

    Update twai_base.h

--- src/util/car/twai_base.h
+++ src/util/car/twai_base.h
@@ -10,6 +10,9 @@
 #include <esp_log.h>
 #include <freertos/queue.h>
 #include <driver/twai.h>
+#include <driver/gpio.h>
+
+#define TWAI_BUS_SIZE 2
 
 class BaseCan {
     public:
@@ -17,14 +20,23 @@ class BaseCan {
         ~BaseCan();
         bool begin_tasks();
         esp_err_t init_state() const;
+        uint8_t aabbcc = 0;
+        twai_handle_t twai_handler[2];
+        twai_handle_t twai_handler2;
+        // uint8_t TWAI_BUS_SIZE = 2;
 
     protected:
         const char* name;
+        
+
         TaskHandle_t tx_task = nullptr;
         TaskHandle_t rx_task = nullptr;
+        // TaskHandle_t tx_task = nullptr;
+        // TaskHandle_t rx_task = nullptr;
         uint8_t tx_time_ms = 0;
-        gpio_num_t can_tx_pin = GPIO_NUM_5;
-        gpio_num_t can_rx_pin = GPIO_NUM_4;
+
+        gpio_num_t can_tx_pin[TWAI_BUS_SIZE] = {GPIO_NUM_5, GPIO_NUM_23};
+        gpio_num_t can_rx_pin[TWAI_BUS_SIZE] = {GPIO_NUM_4, GPIO_NUM_22};
         
 
         uint16_t diag_tx_id = 0;
@@ -42,12 +54,18 @@ class BaseCan {
             static_cast<BaseCan*>(_this)->tx_task_loop();
         }
 
-        virtual void tx_frames(){};
-        virtual void on_rx_frame(uint32_t id,  uint8_t dlc, uint64_t data, uint64_t timestamp) {};
-        virtual void on_rx_done(uint64_t now_ts){};
-        void twai_rx_task(void *arg);
+        static void start_rx_task_loop_bus0(void *_this) {
+            static_cast<BaseCan*>(_this)->rx_task_loop();
+        }
+        static void start_tx_task_loop_bus0(void *_this) {
+            static_cast<BaseCan*>(_this)->tx_task_loop();
+        }
 
-        bool send_messages = true;
+        virtual void tx_frames(uint8_t bus){};
+        virtual void on_rx_frame(uint32_t id,  uint8_t dlc, uint64_t data, uint64_t timestamp, uint8_t bus) {};
+        virtual void on_rx_done(uint64_t now_ts){};
+
+        bool send_messages[TWAI_BUS_SIZE] = {true, true};
         QueueHandle_t* diag_rx_queue;
         twai_status_info_t can_status;
         esp_err_t can_init_status;
